34d1f7b8ab2b490dac0e369d90e2731f53753c9a
guironm <guiro.karavartanian@gmail.com>
2017-06-03 04:31:09 +0300

    filters

---
+++ FootballChallenge/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,61 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-datepicker/www/android/DatePicker.js",
+        "id": "cordova-plugin-datepicker.DatePicker",
+        "clobbers": [
+            "datePicker"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.6",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "ionic-plugin-keyboard": "1.0.9",
+    "cordova-plugin-datepicker": "0.9.2",
+    "cordova-plugin-inappbrowser": "1.7.2-dev",
+    "phonegap-plugin-push": "1.8.4"
+};
+// BOTTOM OF METADATA
+});
